3fb960067c5234f0be584bf7c0ef09daed96bb84
Bojan Kalicanin <kalcho@zoho.com>
2016-12-09 14:34:29 +0100

    removed unneadded stuff from main program

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,9 +19,6 @@ def run_game():
     # Make a ship
     ship = Ship(screen)
 
-    # Set the background color.
-    bg_color = (230, 230, 230)
-
     # Start the main loop for the game.
     while True:
         gf.check_events()
