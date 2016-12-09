b2fbdfc4de08dd4851fb5bf0aa8e45dbb61590a6
Bojan Kalicanin <kalcho@zoho.com>
2016-12-09 12:46:10 +0100

    added game's main program

---
+++ alien_invasion.py
@@ -0,0 +1,35 @@
+# Author: Bojan G. Kalicanin
+# Date: 09-Dec-2016
+# Main Game program
+
+import sys
+import pygame
+from settings import Settings
+
+def run_game():
+    
+    # Initialize game and create a screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Set the background color.
+    bg_color = (230, 230, 230)
+
+    # Start the main loop for the game.
+    while True:
+
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        # Redraw the screen during each pass through the loop.
+        screen.fill(ai_settings.bg_color)
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+run_game()
