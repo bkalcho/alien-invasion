6da6d55a2c03bf336e4d67f64aa2d6e93418afca
Bojan Kalicanin <kalcho@zoho.com>
2016-12-09 13:50:11 +0100

    added ship instance in main program

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
     
@@ -14,6 +15,9 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship
+    ship = Ship(screen)
 
     # Set the background color.
     bg_color = (230, 230, 230)
@@ -28,6 +32,7 @@ def run_game():
         
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
