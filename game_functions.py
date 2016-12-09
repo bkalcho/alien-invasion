4c98fdbb36a18e6555742a19a18e8dd5b1839dfa
Bojan Kalicanin <kalcho@zoho.com>
2016-12-09 14:36:32 +0100

    fixed problem with game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -14,9 +14,9 @@ def check_events():
 
 def update_screen(ai_settings, screen, ship):
     """Update images on the screen and flip to the new screen."""
-        # Redraw the screen during each pass through the loop.
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
+    # Redraw the screen during each pass through the loop.
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
 
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+    # Make the most recently drawn screen visible.
+    pygame.display.flip()
