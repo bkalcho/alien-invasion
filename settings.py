bd39859510b7f95ed851978eb1a02f293b334c24
Bojan Kalicanin <kalcho@zoho.com>
2016-12-09 12:50:42 +0100

    added game's settings module

---
+++ settings.py
@@ -0,0 +1,13 @@
+# Author: Bojan G. Kalicanin
+# Date: 09-Dec-2016
+# Settings module stores all the game settings
+
+class Settings():
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
