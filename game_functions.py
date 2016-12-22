165d588c71cccda4c6e958166b0367152609fa02
Bojan Kalicanin <kalcho@zoho.com>
2016-12-22 17:52:02 +0100

    added increasing game speed by leveling up

--- game_functions.py
+++ game_functions.py
@@ -56,6 +56,8 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
     """Start a new game when the player clicks Play."""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # Reset the game settings.
+        ai_settings.initialize_dynamic_settings()
         # Hide the mouse cursor.
         pygame.mouse.set_visible(False)
         
@@ -114,8 +116,9 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     # Remove any bullets and aliens that have collided.
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
     if len(aliens) == 0:
-        # Destroy existing bullets and create new fleet.
+        # Destroy existing bullets, speedup game, and create new fleet.
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 
